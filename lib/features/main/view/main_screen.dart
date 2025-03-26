011ef3672d96a21cc38d2b02a596fc5df0fea894
vitaliy.kharlan.1977@gmail.com <vitaliy.kharlan.1977.gmail.com>
2025-03-27 00:23:27 +0200

    added design

--- lib/features/main/view/main_screen.dart
+++ lib/features/main/view/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -25,23 +26,202 @@ class _MainScreenState extends State<MainScreen> {
                 colors: [Colors.blue.shade100, Colors.pink.shade100],
               ),
             ),
-            child: Center(
-              child: Text(
-                'MainScreen',
-                style: TextStyle(fontSize: 40),
-                textAlign: TextAlign.center,
-              ),
-            ),
           ),
-          Center(
-            child: Container(
-              width: screenWidth * 0.9,
-              height: screenHeight * 0.9,
-
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-              ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 40,
+                    top: 20,
+                    right: 40,
+                    bottom: 20,
+                  ),
+                  child: IntrinsicHeight(
+                    child: Container(
+                      width: screenWidth * 0.9,
+                      height: screenHeight * 0.8,
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(24),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '''\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\Сучасна '''
+                              '''українська лексикографія, '''
+                              '''відповідаючи вимогам інформаційного суспільства, '''
+                              '''характеризується тенденцією цифрового представлення '''
+                              '''лексикографічних даних. Керуючись актуальними '''
+                              '''завданнями й методами комп'ютерної лінгвістики у '''
+                              '''галузі словникарства, студент-бакалавр 4-го курсу '''
+                              '''Ярох Анна досліджує морфонологічні процеси в '''
+                              ''''"Словнику українських морфем" Л.М. Полюги. Вона '''
+                              '''також працює над проєктом його електронної версії, '''
+                              '''що сприятиме подальшому розвитку цифрової лексикографії.''',
+                              style: GoogleFonts.roboto(
+                                fontSize: 20,
+                                height: 2,
+                                textBaseline: TextBaseline.alphabetic,
+                              ),
+                              textAlign: TextAlign.justify,
+                              strutStyle: const StrutStyle(
+                                forceStrutHeight: true,
+                                height: 2.5,
+                              ),
+                            ),
+                            Text(
+                              '''\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\Важливість '''
+                              '''цієї проблеми полягає у необхідності оптимізації '''
+                              '''обробки та аналізу мовних даних, що сприятиме '''
+                              '''підвищенню ефективності досліджень та розширенню '''
+                              '''можливостей користувачів. Складність конвертації '''
+                              '''традиційних морфемних словників, таких як '''
+                              '''"Морфемний аналіз" Яценка І.Т. та '''
+                              ''''"Словник українських морфем" Л.М. Полюги, '''
+                              '''обумовлена спеціальним способом подання морфемної '''
+                              '''структури слів. Через це актуальним є розроблення '''
+                              '''методики й програмного забезпечення для автоматичної '''
+                              '''конверсії "Словника українських морфем" у цифровий формат.''',
+                              style: GoogleFonts.roboto(
+                                fontSize: 20,
+                                height: 2,
+                                textBaseline: TextBaseline.alphabetic,
+                              ),
+                              textAlign: TextAlign.justify,
+                              strutStyle: const StrutStyle(
+                                forceStrutHeight: true,
+                                height: 2.5,
+                              ),
+                            ),
+                            Text.rich(
+                              TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text:
+                                        '\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0Мета дослідження ',
+                                    style: GoogleFonts.roboto(
+                                      fontSize: 20,
+                                      height: 2,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text:
+                                        'полягає у створенні електронної версії "Словника українських морфем" '
+                                        'Л.М. Полюги з акцентом на морфонологічні процеси.',
+                                    style: GoogleFonts.roboto(
+                                      fontSize: 20,
+                                      height: 2,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              textAlign: TextAlign.justify,
+                              strutStyle: const StrutStyle(
+                                forceStrutHeight: true,
+                                height: 2.5,
+                              ),
+                            ),
+                            Text(
+                              '''\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\Завдання дослідження: ''',
+                              style: GoogleFonts.roboto(
+                                fontSize: 20,
+                                height: 2,
+                                fontWeight: FontWeight.bold,
+                                textBaseline: TextBaseline.alphabetic,
+                              ),
+                              textAlign: TextAlign.justify,
+                              strutStyle: const StrutStyle(
+                                forceStrutHeight: true,
+                                height: 2.5,
+                              ),
+                            ),
+                            Text(
+                              '''\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\➢ '''
+                              '''дослідити історію розвитку комп’ютерної лексикографії; ''',
+                              style: GoogleFonts.roboto(
+                                fontSize: 20,
+                                height: 2,
+                                textBaseline: TextBaseline.alphabetic,
+                              ),
+                              textAlign: TextAlign.justify,
+                              strutStyle: const StrutStyle(
+                                forceStrutHeight: true,
+                                height: 2.5,
+                              ),
+                            ),
+                            Text(
+                              '''\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\➢ '''
+                              '''визначити актуальність комп’ютерної лексикографії, описати її завдання та проблеми;''',
+                              style: GoogleFonts.roboto(
+                                fontSize: 20,
+                                height: 2,
+                                textBaseline: TextBaseline.alphabetic,
+                              ),
+                              textAlign: TextAlign.justify,
+                              strutStyle: const StrutStyle(
+                                forceStrutHeight: true,
+                                height: 2.5,
+                              ),
+                            ),
+                            Text(
+                              '''\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\➢ '''
+                              '''розробити концепції інфологічної та даталогічної моделі лексикографічної бази даних;''',
+                              style: GoogleFonts.roboto(
+                                fontSize: 20,
+                                height: 2,
+                                textBaseline: TextBaseline.alphabetic,
+                              ),
+                              textAlign: TextAlign.justify,
+                              strutStyle: const StrutStyle(
+                                forceStrutHeight: true,
+                                height: 2,
+                              ),
+                            ),
+                            Text(
+                              '''\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\➢ '''
+                              '''описати призначення словника та вихідні дані для його розробки;''',
+                              style: GoogleFonts.roboto(
+                                fontSize: 20,
+                                height: 2,
+                                textBaseline: TextBaseline.alphabetic,
+                              ),
+                              textAlign: TextAlign.justify,
+                              strutStyle: const StrutStyle(
+                                forceStrutHeight: true,
+                                height: 2.5,
+                              ),
+                            ),
+                            Text(
+                              '''\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\➢ '''
+                              '''обрати програмний інструментарій для конвертації словника;''',
+                              style: GoogleFonts.roboto(
+                                fontSize: 20,
+                                height: 2,
+                                textBaseline: TextBaseline.alphabetic,
+                              ),
+                              textAlign: TextAlign.justify,
+                              strutStyle: const StrutStyle(
+                                forceStrutHeight: true,
+                                height: 2.5,
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
--- lib/features/word_search/view/word_search_screen.dart
+++ lib/features/word_search/view/word_search_screen.dart
@@ -60,54 +60,65 @@ class _WordSearchScreenState extends State<WordSearchScreen> with RouteAware {
     final screenWidth = MediaQuery.sizeOf(context).width;
     final screenHeight = MediaQuery.sizeOf(context).height;
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Colors.blue.shade100, Colors.pink.shade100],
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.blue.shade100, Colors.pink.shade100],
+              ),
             ),
           ),
-          child: Padding(
+          Padding(
             padding: EdgeInsets.symmetric(
-              horizontal: screenWidth * 0.1,
+              horizontal: screenWidth * 0.25,
               vertical: screenHeight * 0.1,
             ),
-            child: Column(
-              children: [
-                TextField(
-                  controller: _searchController,
-                  onChanged: _searchWord,
-                  decoration: InputDecoration(
-                    labelText: 'Пошук слова',
-                    labelStyle: TextStyle(
-                      color: Colors.black.withAlpha(100),
-                      fontSize: 16,
+            child: Container(
+              width: screenWidth * 0.9,
+              height: screenHeight * 0.8,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _searchController,
+                    onChanged: _searchWord,
+                    decoration: InputDecoration(
+                      labelText: 'Пошук слова',
+                      labelStyle: TextStyle(
+                        color: Colors.black.withAlpha(100),
+                        fontSize: 16,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide:
+                            const BorderSide(color: Colors.grey, width: 1),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.search),
+                        onPressed: () => _searchWord(_searchController.text),
+                      ),
                     ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide:
-                          const BorderSide(color: Colors.grey, width: 1),
-                    ),
-                    suffixIcon: IconButton(
-                      icon: const Icon(Icons.search),
-                      onPressed: () => _searchWord(_searchController.text),
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Expanded(child: _buildSearchResults()),
-              ],
+                  const SizedBox(height: 20),
+                  Expanded(child: _buildSearchResults()),
+                ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
